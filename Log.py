a3d70ae90521f527231a8238697cfed4b9c98ee8
kdominiaq <dominiam1998@gmail.com>
2021-11-07 10:26:39 +0100

    Add Log.py which allows generating logging file. It will help to find bugs and troubleshoots.

---
+++ Log.py
@@ -0,0 +1,75 @@
+"""
+Class creates logging holder and file to contain all logs.
+        
+"""
+
+import logging 
+import os
+import pathlib
+
+from time import gmtime, strftime, sleep
+
+class Log:
+    def __init__(self) -> None:
+
+        self._logs_folder()
+        logging.basicConfig(filename='logs/{}.log'.format(self._get_current_time()), 
+                            format='%(levelname)s: %(asctime)s: %(message)s', 
+                            level=logging.INFO)
+
+    @staticmethod
+    def start():
+        """
+        
+        """
+        try:
+            username = os.getlogin()
+            message = "Log started, enjoy the game {}!".format(username)
+            logging.info(message)
+        except Exception as e:
+            return -1
+
+
+    @staticmethod
+    def _logs_folder():
+        """
+        Create a logs directory if it doesn't exist.
+        :param: None.
+        :return: None.
+        """
+
+        # get absolute path to project
+        try:
+            absolute_path = pathlib.Path().resolve()
+            direciory_name = "logs"
+
+            # create final the path with directory name
+            path = os.path.join(absolute_path, direciory_name)
+            is_exist = os.path.exists(path)
+            print(path, is_exist)
+            # if the directory does not exist create it
+            if not is_exist:
+                os.makedirs(path)
+                
+        except Exception:
+            return -1
+
+
+    def _get_current_time(self):
+        """
+        Return current time, sytnax is:
+        Year-Month-Day (Hour:Minute:Second)
+        2000-01-01 (10:12:45)
+
+        :param: none
+        :return: current time
+        """
+
+        try: 
+          current_time = strftime("%Y-%m-%d (%H:%M:%S)", gmtime())
+
+        except Exception as e:
+            return -1
+
+        return current_time
+
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import time
+import logging
+from Log import Log
 
-import cv2
-import numpy as np
-from mss import mss
+log = Log()
+log.start()
 
+def main():
+    pass
 
-def record(name):
-    with mss() as sct:
-        mon = sct.monitors[0]
-
-        while True:              
-            img = sct.grab(mon)
-            cv2.imshow('test', np.array(img))
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
-
-record("Video")
+if '__main__' == __name__:
+    main()
