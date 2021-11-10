84c22ad8d346ff140cc6c5fbfb989c5803dd343e
kdominiaq <dominiam1998@gmail.com>
2021-11-10 20:12:03 +0100

    Improving find_chessboard function (file: capture_screen) by add raise Exceptin ValueError if chessboard is not found. Reorganize main file

--- capture_screen.py
+++ capture_screen.py
@@ -27,40 +27,28 @@ class CaptureScreen:
         :param: None
         :return: np.array of the image 
         """
-        top_left_y = 0
-        bot_right_y = 0
-        top_left_x = 0
-        bot_right_x = 0
         with mss() as sct:
             try:
                 mon = sct.monitors[0]
                 img = sct.grab(mon)
                 img = np.array(img)
-                top_left_y, bot_right_y, top_left_x, bot_right_x = self.find_chessboard(img)
+                return img
+
             except Exception as e:
-                logging.error("Monitor is not available, check Yours computer.")
+                logging.error("Monitor is not available, check Your computer.")
                 return -1
-           
-            while True:
-                img = sct.grab(mon) 
-                img = np.array(img)
+        
 
-                cv2.imshow('test', img[top_left_y:bot_right_y,top_left_x:bot_right_x])
-                if cv2.waitKey(0):
-                    cv2.destroyAllWindows()
-
-
-
-    def find_chessboard(self, img) -> List(int):
+    def find_chessboard(self, img):
         """
-        Find chessboard by searching corners, the function has two implications, there are depend on the color border, more information in the next lines of function
-
+        Find chessboard by searching corners, the function has two implications, there are depend on the color border, more information in the next lines of function. Return coordinates of top left corner saved like tuple(y, x), size of the chessboard tuple((height_of_the_chessboard, width_of_the_chessboard) and 
+                 size of the field on the chessboard tuple((height_of_the_field, width_of_the_field)
 
         :param img: the image of the screen
-        :return List: contains coordinates of top left corner and bot right corner of the found chessboard
+        :return: tuple(y, x), tuple((height_of_the_chessboard, width_of_the_chessboard), tuple((height_of_the_field, width_of_the_field)
         """
 
-
+        print(img.shape)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 160, 200, cv2.THRESH_BINARY_INV)[1]
 
@@ -79,60 +67,53 @@ class CaptureScreen:
         # - 7 -> dark_border
         type_of_border = None
 
-        # if the chessboard is found break the loops
         is_found = False
 
         # if corners are found break the loop and pass the type_of_board to next code
-        while not is_found:
-            for border in borders:
-                is_found, corners = cv2.findChessboardCorners(thresh, border, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-                if is_found:
-                    # saved the first element from the tuple
-                    type_of_border = border[0]
-                    break
-                       
-        # looking for top left corner(tp) of the 
-        field_height, field_width = 0, 0
+        for border in borders:
+            is_found, corners = cv2.findChessboardCorners(thresh, border, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+            if is_found:
+                # saved the first element from the tuple
+                type_of_border = border[0]
+                break
         
-        # coordinates of the top left corner are saved in the first element form corners list, next corner is saved in next index
-        top_left_x, top_left_y = corners[0][0]
-        next_to_top_left_corner_x,_ = corners[1][0]
+        if is_found:
+            # looking for top left corner(tp) of the 
+            field_height, field_width = 0, 0
+            
+            # coordinates of the top left corner are saved in the first element form corners list, next corner is saved in next index
+            top_left_x, top_left_y = corners[0][0]
+            next_to_top_left_corner_x,_ = corners[1][0]
 
-        # the second element from the corners list contains (x, y), but this time x value is increased by width field, 
-        # so the width of the field will be, result from subtracting the second corner with the first corner (only for x values)
-        # width and height are equal because the field is the square        
-        field_width = next_to_top_left_corner_x - top_left_x
-        field_height = field_width
-        
-        # cutting image to display only chessboard, the top left corner can be in two places (beetwen % mark):
-        # - dark_border:        - bright_border
-        #   X X . . . . . .       . . . . . . . .
-        #   X X . . . . . .       . X X . . . . .
-        #   . . . . . . . .       . X X . . . . . 
-        #   . . . . . . . .       . . . . . . . .
-        #   . . . . . . . .       . . . . . . . .
-        #   . . . . . . . .       . . . . . . . .    
-        #   . . . . . . . .       . . . . . . . .
-        #   . . . . . . . .       . . . . . . . . 
+            # the second element from the corners list contains (x, y), but this time x value is increased by width field, 
+            # so the width of the field will be, result from subtracting the second corner with the first corner (only for x values)
+            # width and height are equal because the field is the square        
+            field_width = next_to_top_left_corner_x - top_left_x
+            field_height = field_width
+            
+            # cutting image to display only chessboard, the top left corner can be in two places (beetwen % mark):
+            # - dark_border:        - bright_border
+            #   X X . . . . . .       . . . . . . . .
+            #   X X . . . . . .       . X X . . . . .
+            #   . . . . . . . .       . X X . . . . . 
+            #   . . . . . . . .       . . . . . . . .
+            #   . . . . . . . .       . . . . . . . .
+            #   . . . . . . . .       . . . . . . . .    
+            #   . . . . . . . .       . . . . . . . .
+            #   . . . . . . . .       . . . . . . . . 
 
-        # saving information about found chessboard, which border is present :
-        # - 5 -> bright_border
-        # - 7 -> dark_border
-        if type_of_border == 5:
-            top_left_x -= 2 * field_width
-            top_left_y -= 2 * field_height
-        elif type_of_border == 7:
-            top_left_x -= field_width
-            top_left_y -= field_height
-        
-        bot_right_x, bot_right_y = top_left_x + field_width * 8, top_left_y+ field_height * 8
-        bot_right_x, bot_right_y, top_left_x, top_left_y = int(bot_right_x), int(bot_right_y), int(top_left_x), int(top_left_y)
-        
+            # saving information about found chessboard, which border is present :
+            # - 5 -> bright_border
+            # - 7 -> dark_border
+            if type_of_border == 5:
+                top_left_x -= 2 * field_width
+                top_left_y -= 2 * field_height
+            elif type_of_border == 7:
+                top_left_x -= field_width
+                top_left_y -= field_height
+            
+            chessboard_width, chessboard_height = field_width * 8, field_height * 8       
 
-        return [top_left_y, bot_right_y, top_left_x, bot_right_x]
-
-
-        
-        
-
-CaptureScreen().grab_screen()
+            return (int(top_left_y), int(top_left_x)), (int(chessboard_width), int(chessboard_height)), (int(field_height) ,int(field_width))
+        else:
+            raise ValueError("Chessboard not found")           
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-import logging
-from Log import Log
+"""
+opencv save imgaes as [len(y), len(x)]
+"""
 
+import logging
+import cv2
+from Log import Log
+from capture_screen import CaptureScreen
 
 # start logging, now every exception will be save in the file
 Log()
 
+# holder to the screen, allow to caprtue the current screen which is displeyed
+screen_holder = CaptureScreen()
 
 def main():
-    pass
+    
+    # grab currend screen
+    while True:
+        try:
+            image_of_screen = screen_holder.grab_screen()
+
+            # getting coordinates_of_chessboard and size_of_the_field
+            coordinates_of_chessboard, size_of_the_chessboard , size_of_the_field = screen_holder.find_chessboard(image_of_screen)
+            top_left_y, top_left_x = coordinates_of_chessboard
+            chessboard_height, chessboard_width = size_of_the_chessboard
+
+            print(image_of_screen[0:1,0:1])
+            cv2.imshow('test', image_of_screen[top_left_y:top_left_y + chessboard_height,top_left_x:top_left_x + chessboard_width])
+            if cv2.waitKey(0):
+                cv2.destroyAllWindows()
+        except ValueError:
+            print('Can not find chessboard, please run the game on computer and make it displayed')
+
 
 if '__main__' == __name__:
     main()
