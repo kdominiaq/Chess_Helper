924f30341027a76b319e4381e9a9cf6c9f86d680
kdominiaq <dominiam1998@gmail.com>
2021-11-11 12:26:59 +0100

    Add Execeptions and display found chessbaord.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import logging
 import cv2
 from Log import Log
 from capture_screen import CaptureScreen
+from chessboard import ChessBoard
+from time import sleep
+from exceptions import ScreenNotBeFound, ChessBoardNotFound
 
 # start logging, now every exception will be save in the file
 Log()
@@ -13,26 +16,47 @@ Log()
 # holder to the screen, allow to caprtue the current screen which is displeyed
 screen_holder = CaptureScreen()
 
+
+# init chessboard
+chessboard = ChessBoard()
+
+
 def main():
     
     # grab currend screen
     
     while True:
+
         try:
             image_of_screen = screen_holder.grab_screen()
+        except ScreenNotBeFound:
+            # give 2 seconds to do some operations on computer to for example run website to play chessbar
+            sleep(2)
+            # skip next lines of the code
+            continue
 
-            # get information about chessboard
-            coordinates_of_chessboard, size_of_the_chessboard , size_of_the_field = screen_holder.find_chessboard(image_of_screen)
-            top_left_y, top_left_x = coordinates_of_chessboard
-            chessboard_height, chessboard_width = size_of_the_chessboard
 
-            # displat test img od the chessboard
-            cv2.imshow('test', image_of_screen[top_left_y:top_left_y + chessboard_height,top_left_x:top_left_x + chessboard_width])
-            if cv2.waitKey(0):
-                cv2.destroyAllWindows()
-        # Exception throw by find_chessboad()
-        except ValueError:
-            print('Can not find chessboard, please run the game on computer and make it displayed')
+        # find chessboard
+        chessboard.find(image_of_screen)
+
+        # try to get chessboad parameters
+        try: 
+            coordinates_of_chessboard = chessboard.get_cb_coordianates
+            size_of_the_chessboard = chessboard.get_cb_size
+            size_of_the_field = chessboard.get_f_size
+        except ChessBoardNotFound as msg:
+            logging.error(msg)
+            continue
+            
+        
+        top_left_y, top_left_x = coordinates_of_chessboard
+        chessboard_height, chessboard_width = size_of_the_chessboard
+
+        # displat test img od the chessboard
+        cv2.imshow('test', image_of_screen[top_left_y:top_left_y + chessboard_height,top_left_x:top_left_x + chessboard_width])
+        if cv2.waitKey(0):
+            cv2.destroyAllWindows()
+
 
 
 if '__main__' == __name__:
